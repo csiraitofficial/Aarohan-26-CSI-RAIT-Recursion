f7f98578f77a2d774ba0e5a8b1b1e60327bed558
Chiragchaudhari2005 <chaudharichirag3006@gmail.com>
2026-03-06 20:00:57 +0530

    Removed old curo-frontend folder

--- curo-frontend/src/types/Medicine.ts
+++
@@ -1,9 +0,0 @@
-export interface Medicine {
-    id: number;
-    name: string;
-    dosage: string;
-    time: string;
-    days: string[];
-  }
-  
-  export type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
